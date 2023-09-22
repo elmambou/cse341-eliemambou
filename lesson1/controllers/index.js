b0bd93bfba8c7ade76227b407564552f00f68fe2
elmambou <esmambou@gmail.com>
2023-09-23 01:20:17 +0200

    update latest changes

--- lesson1/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const awesomeFunction = (req, res, next) => {
-  res.json('Marie Antoinette');
-};
-
-const GenerateAnotherEntry = (req, res, next) => {
-    res.json('Very important person');
-  };
-
-module.exports = { awesomeFunction, GenerateAnotherEntry };
